a4cfa413ab6da09d7c0479030c358fc60c50031a
Jessica Schaefer <jschaefer@3pc.de>
2020-05-18 15:53:27 +0200

    question/answer logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,13 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params["question"]
-    answer = params
+
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?");
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
